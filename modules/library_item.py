9a87d7d3dc6927754faa66e9f54c163ae3732e79
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-12 00:25:32 +0300

    Wrote the initial structure of [LibraryItem] class

--- modules/library_item.py
+++ modules/library_item.py
@@ -1,1 +1,15 @@
- 
+from abc import ABC, abstractmethod
+
+class LibraryItem(ABC):
+    # FIXME: add the attributes for [LibraryItem]
+    def __init__(self) -> None:
+        # FIXME: initialize the attributes of [LibraryItem]
+        super().__init__()
+
+    @abstractmethod
+    def display_info(self):
+        pass
+
+    @abstractmethod
+    def check_availability(self):
+        pass
