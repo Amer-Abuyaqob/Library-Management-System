74abdd3f17819e3a68b3ad3ac85c9f49780749dc
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-12 23:51:37 +0300

    Wrote the initial structure of [Reservable] class

--- modules/reservabale.py
+++ modules/reservabale.py
@@ -1,1 +1,12 @@
- 
+from abc import ABC, abstractmethod
+from user import User
+
+class Reservable(ABC):
+    # FIXME: add the attributes for [Reservable]
+    def __init__(self):
+        # FIXME: initialize the attributes of [Reservable]
+        super().__init__()
+
+    @abstractmethod
+    def reserve(self, user: User):
+        pass
