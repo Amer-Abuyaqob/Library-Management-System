85fa040e2114202ebb683cea6c70094bbf81314c
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-13 00:10:22 +0300

    Wrote the initial structure of [DVD] class

--- modules/dvd.py
+++ modules/dvd.py
@@ -1,1 +1,8 @@
- 
+from library_item import LibraryItem
+from reservabale import Reservable
+
+class DVD(LibraryItem, Reservable):
+    # FIXME: add the attributes for []
+    def __init__(self) -> None:
+        # FIXME: initialize the attributes of []
+        super().__init__()
