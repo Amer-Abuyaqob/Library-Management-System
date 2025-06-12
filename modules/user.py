9df2be70235b3442532668774e818cf9e0c3652e
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-12 23:38:32 +0300

    Wrote the initial structure of [User] class

---
+++ modules/user.py
@@ -0,0 +1,5 @@
+class User:
+    # FIXME: add the attributes for [User]
+    def __init__(self) -> None:
+        # FIXME: initialize the attributes of [User]
+        pass # HACK: only a place holder
