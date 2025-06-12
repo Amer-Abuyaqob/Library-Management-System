92b28a630ae59b54cfa4c2996a78b125ea67484e
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-13 00:09:57 +0300

    Wrote the initial structure of [Magazine] class

--- modules/magazine.py
+++ modules/magazine.py
@@ -1,1 +1,8 @@
- 
+from library_item import LibraryItem
+from reservabale import Reservable
+
+class Magazine(LibraryItem, Reservable):
+    # FIXME: add the attributes for []
+    def __init__(self) -> None:
+        # FIXME: initialize the attributes of []
+        super().__init__()
