5929046a478d00439acb4acd0c25a7452023337c
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-20 16:48:30 +0300

    Implemented Magazine class with attributes, display_info, and check_availability methods

--- modules/magazine.py
+++ modules/magazine.py
@@ -2,7 +2,21 @@ from library_item import LibraryItem
 from reservabale import Reservable
 
 class Magazine(LibraryItem, Reservable):
-    # FIXME: add the attributes for []
-    def __init__(self) -> None:
-        # FIXME: initialize the attributes of []
-        super().__init__()
+    def __init__(self, title, author, year, available, genre):
+        super().__init__(title, author, year, available)
+        self.__genre = genre
+        self.__reserved = None
+
+    @property
+    def genre(self):
+        return self.__genre
+
+    @genre.setter
+    def genre(self, genre):
+        self.__genre = genre
+
+    def display_info(self):
+        return f"Item type: Magazine \n{super().display_info()} \nGenre: {self.__genre}"
+    
+    def check_availability(self):
+        return self.__available
