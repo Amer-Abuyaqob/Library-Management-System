727bbd55942fab9455e811803d4ff15d0b07c52a
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-20 15:56:02 +0300

    Added LibraryItem class getters

--- modules/library_item.py
+++ modules/library_item.py
@@ -8,6 +8,22 @@ class LibraryItem(ABC):
         self.__year = int(year)
         self.__available = bool(available)
 
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def available(self):
+        return self.__available
+
     @abstractmethod
     def display_info(self):
         pass
