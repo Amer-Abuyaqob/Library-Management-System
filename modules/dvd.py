e6720030c02b6d57028318667f1d9d4b457fc189
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-20 16:47:07 +0300

    Added attributes, methods, and reservation functionality to DVD class

--- modules/dvd.py
+++ modules/dvd.py
@@ -1,8 +1,34 @@
 from library_item import LibraryItem
+from modules.user import User
 from reservabale import Reservable
 
 class DVD(LibraryItem, Reservable):
-    # FIXME: add the attributes for []
-    def __init__(self) -> None:
-        # FIXME: initialize the attributes of []
-        super().__init__()
+    def __init__(self, title, author, year, available, duration):
+        super().__init__(title, author, year, available)
+        self.__duration = int(duration) # duration of the dvd content in minutes
+        self.__reserved = None
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    @duration.setter
+    def duration(self, duration):
+        self.__duration = duration
+
+    def display_info(self):
+        return f"Item type: DVD \n{super().display_info()} \nDuration: {self.__duration} minutes"
+    
+    def check_availability(self):
+        return self.__available
+    
+    def reserve(self, user: User):
+        """
+        Reserve the DVD for a specific user.
+
+        Args:
+            user (User): The user who is reserving the DVD.
+
+        Sets the __reserved attribute to the given user, indicating that the DVD is reserved by this user.
+        """
+        self.__reserved = user
