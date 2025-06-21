d44fec8f01f0214703fcec5c85c25936e1886af7
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-21 17:13:49 +0300

    Implemented items_options method, TODOs for menu options

--- modules/main.py
+++ modules/main.py
@@ -4,15 +4,29 @@ class Main:
     def __init__(self):
         library = Library()
     
+    def items_options(self):
+        # FIXME: exeption handling for user's option
+        main_option = int(input("Your Choice is: "))
+        match main_option:
+            case 1:
+                pass # TODO: self.items_view_menu()
+            case 2:
+                pass # TODO: self.items_add_menu()
+            case 3:
+                pass # TODO: self.items_remove_menu()
+            case 4:
+                pass # TODO: self.items_update_menu()
+            case 5:
+                pass # TODO: BACK
 
-
-    def item_menu(self):
+    def items_menu(self):
         print("Items Menu")
         print("1- View items")
         print("2- Add items")
         print("3- Remove items")
         print("4- Update items")
         print("5- Back")
+        self.items_option()
 
     def main_option(self):
         # FIXME: exeption handling for user's option
@@ -33,6 +47,7 @@ class Main:
         print("2- Users Menu")
         print("3- Borrow/Return Menu")
         print("4- Save and Exit")
+        self.main_option()
 
     def run(self):
         print("Welcome to LMS")
