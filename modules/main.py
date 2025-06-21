065d2ec404a7854edb517a7db4759337deba917a
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-21 16:59:23 +0300

    Wrote main_option

--- modules/main.py
+++ modules/main.py
@@ -4,6 +4,19 @@ class Main:
     def __init__(self):
         library = Library()
     
+    def main_option(self):
+        # FIXME: exeption handling for user's option
+        main_option = int(input("Your Choice is: "))
+        match main_option:
+            case 1:
+                self.items_menu()
+            case 2:
+                self.users_menu()
+            case 3:
+                self.borrow_return_menu()
+            case 4:
+                self.end()
+
     def main_menu(self):
         print("Main Menu")
         print("1- Items Menu")
