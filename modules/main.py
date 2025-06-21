ac22ca123da026ac19fe23a7ac4ecde6c777b09a
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-06-21 16:58:49 +0300

    Wrote main menu

--- modules/main.py
+++ modules/main.py
@@ -5,7 +5,12 @@ class Main:
         library = Library()
     
     def main_menu(self):
-        print()
+        print("Main Menu")
+        print("1- Items Menu")
+        print("2- Users Menu")
+        print("3- Borrow/Return Menu")
+        print("4- Save and Exit")
 
     def run(self):
+        print("Welcome to LMS")
         self.main_menu()
