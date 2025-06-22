eb9d551fb1d02f60e146f352617d7511f1280b5c
Amer Abuyaqob <amer.abuyaqob@gmail.com>
2025-06-22 03:17:50 +0300

    Add custom exceptions and replace ValueError

---
+++ modules/exceptions.py
@@ -0,0 +1,23 @@
+class LibraryError(Exception):
+    """Base class for library related exceptions."""
+    pass
+
+
+class ItemNotFoundError(LibraryError):
+    """Raised when an item is not found in the library."""
+    pass
+
+
+class UserNotFoundError(LibraryError):
+    """Raised when a user is not found in the library."""
+    pass
+
+
+class ItemNotAvailableError(LibraryError):
+    """Raised when an item is not available for borrowing."""
+    pass
+
+
+class ItemNotBorrowedError(LibraryError):
+    """Raised when a user tries to return an item they haven't borrowed."""
+    pass
